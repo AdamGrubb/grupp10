c435e7675a0047a6551a0dbbb948d93be1dbd6bf
Johannes Ragnarsson <johannes.ragnarsson@gmail.com>
2023-04-05 16:30:10 +0200

    Resolved merge conflict in tailwind.config.js

--- MegaBite/tailwind.config.js
+++ MegaBite/tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index",
-  "./src/**/*.{js,ts,jsx,tsx}"
-  ],
-
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      headerColor: "#9FBB81",
+      footerColor: "#9FBB81",
+      searchAreaColor: "#AAD37D",
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};
