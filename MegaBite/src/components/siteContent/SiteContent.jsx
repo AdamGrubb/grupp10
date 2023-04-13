a79e9314c2c77f2dc70a8248f277a168001a3457
Jonathan Strand <jontepwn@gmail.com>
2023-04-13 15:48:18 +0200

    search results skal gjord. filter gjord. och annat stuff.

--- MegaBite/src/components/siteContent/SiteContent.jsx
+++ MegaBite/src/components/siteContent/SiteContent.jsx
@@ -1,11 +1,13 @@
 import Search from "../search/Search";
 import RecipeSuggestions from "../recipeSuggestions/RecipeSuggestion";
+import SearchResults from "../searchResults/searchResults";
 
 export default function SiteContent() {
   return (
     <>
       <Search />
-      <RecipeSuggestions />
+      {/* <RecipeSuggestions /> */}
+      <SearchResults />
     </>
   );
 }
