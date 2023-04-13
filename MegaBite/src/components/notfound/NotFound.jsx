933ff943b6a71fba952fe1596b99d458c46c34cb
Johannes Ragnarsson <johannes.ragnarsson@gmail.com>
2023-04-13 10:34:34 +0200

    added the logic for NotFound page

---
+++ MegaBite/src/components/notfound/NotFound.jsx
@@ -0,0 +1,23 @@
+import { useNavigate, Link } from "react-router-dom";
+import "./NotFound.css";
+
+export const NotFound = () => {
+  const navigate = useNavigate();
+  return (
+    <div id="error-page" className="flex flex-col ">
+      <div className="text-center Error">
+        <h1>Oops!</h1>
+        <p>Sorry, an unexpected error has occurred.</p>
+
+        <button
+          onClick={() => {
+            navigate("/");
+          }}
+          className="btnError rounded-full bg-black text-white"
+        >
+          To Homepage
+        </button>
+      </div>
+    </div>
+  );
+};
