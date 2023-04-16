542f726ab9bfb4acb3e4a39edae1db72c06d2deb
Johannes Ragnarsson <johannes.ragnarsson@gmail.com>
2023-04-16 22:23:25 +0200

    linking problem resolved

--- MegaBite/src/components/header/Header.jsx
+++ MegaBite/src/components/header/Header.jsx
@@ -7,15 +7,15 @@ export function Header() {
   return (
     <>
       <header className="header h-24 bg-headerColor">
-        <Link>
+        <Link to="/">
           <img src={logo} className="header-logo" alt="MegaBite logo" />
         </Link>
-        <div className="header-title">
-          <Link to="/">
+        <Link className="header-title" to="/">
+          <div>
             <h4 className="header-upper-font">MegaBite</h4>
             <h4 className="header-lower-font">One bite at a time</h4>
-          </Link>
-        </div>
+          </div>
+        </Link>
       </header>
     </>
   );
