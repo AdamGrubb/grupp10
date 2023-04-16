16a3d07f1c9384bda983fb3126079f5eae146006
Johannes Ragnarsson <johannes.ragnarsson@gmail.com>
2023-04-16 22:17:25 +0200

    added fonts to Header as Figma with some css

--- MegaBite/src/components/header/Header.jsx
+++ MegaBite/src/components/header/Header.jsx
@@ -1,21 +1,21 @@
 import logo from "../../assets/logo1.png";
 import { Link } from "react-router-dom";
 import "../../index.css";
+import "./Header.css";
 
 export function Header() {
   return (
     <>
-      <header className="flex header justify-between items-center bg-headerColor">
-        <Link to="/">
-          <div>
-            <img src={logo} alt="MegaBiteLogo" className="h-24" />
-          </div>
+      <header className="header h-24 bg-headerColor">
+        <Link>
+          <img src={logo} className="header-logo" alt="MegaBite logo" />
         </Link>
-        <Link to="/">
-          <div>MegaBite</div>
-        </Link>
-
-        <div>Menu</div>
+        <div className="header-title">
+          <Link to="/">
+            <h4 className="header-upper-font">MegaBite</h4>
+            <h4 className="header-lower-font">One bite at a time</h4>
+          </Link>
+        </div>
       </header>
     </>
   );
