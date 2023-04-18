efe5d1aafec695a66e7d120a15a8136b767a0d0b
Grandelius <jonatan.grandelius@iths.se>
2023-04-18 10:51:05 +0200

    created recipePage

--- MegaBite/src/components/recipePage/RecipePage.jsx
+++ MegaBite/src/components/recipePage/RecipePage.jsx
@@ -1,34 +1,57 @@
 import porridge from "../../assets/porridge.jpg";
 import RecipeSuggestions from "../recipeSuggestions/RecipeSuggestion";
-import { useNavigate } from "react-router-dom";
 import "./RecipePage.css";
 export default function RecipePage() {
-  const navigate = useNavigate();
-  return (
-    // Denna renderas mellan header & footer på huvusidan. Article kommer vara diven som skyfflar om sectionerna beroende på storlek på skärmen så de hamnar rätt.
-    // Förslagsvis skulle denna kunna ta emot ett recept med all info som prop och sen fördela ut informationen på sektionerna.
-    // h3-taggarna är placeholders och ska ersättas av lämpliga element och upplägg t ex UL för ingredienser osv. Id är placeholder för att hålla reda på vilka delar av figman de motsvarar.
-    //
-    <>
+  
+  
+  const recipe = {
+  title: "Oatmeal porridge",
+  ingredients: ["havregryn", "vatten", "salt", "salt", "salt", "salt","havregryn", "vatten", "salt", "salt", "salt"],
+  description: "Koka upp vatten och havregryn och rör om under tiden.",
+  time: 12,
+  portions: 4,
+  allergies: ["egg", "vegan"]
+};
+
+
+
+return (
+  // Denna renderas mellan header & footer på huvusidan. Article kommer vara diven som skyfflar om sectionerna beroende på storlek på skärmen så de hamnar rätt.
+  // Förslagsvis skulle denna kunna ta emot ett recept med all info som prop och sen fördela ut informationen på sektionerna.
+  // h3-taggarna är placeholders och ska ersättas av lämpliga element och upplägg t ex UL för ingredienser osv. Id är placeholder för att hålla reda på vilka delar av figman de motsvarar.
+  // 
+  
+  <>
       <article>
-        <button className="btnGoBack" onClick={() => navigate(-1)}>
-          Go Back
-        </button>
-        <img id="pictureOfDish" src={porridge} alt="Picture of dish" />
-
-        <h2 id="dishName">Name of the dish</h2>
-
-        <section id="ServingsAndAllergens">
-          <h3>IMPORTERA Nutrition Facts</h3>
+      <div className="flex flex-col content-center" >
+        <img className="img" id="pictureOfDish" src={porridge} alt="Picture of dish" />
+        <h1 className="recipeTitle">{recipe.title}</h1>
+        <section id="ServingsAndAllergens" className="">
+          <ul style={{listStyle: 'disc'}} className="info bg-receptDescriptionColor">
+            <li>portions:{recipe.portions}</li>
+            <li>allergies: {recipe.allergies}</li>
+            <li>time:{recipe.time}</li>
+            </ul>
         </section>
 
-        <section id="ingredients">
-          <h3>IMPORTERA ingredients</h3>
+        <section className="ingredients bg-receptDescriptionColor">
+          <h2 id="big">Ingredients:</h2>
+        <ul style={{listStyle: 'disc'}}> 
+          {recipe.ingredients.map(item => ( <li key={item}>{item}</li> ))}
+           </ul>
         </section>
+      
 
-        <section id="cooking">
-          <h3>IMPORTERA How to Make</h3>
+
+        <section className= "description bg-receptDescriptionColor">
+          <h2 id="big">Description:</h2>
+          <ul>
+            <li>
+              <p>{recipe.description}</p>
+            </li>
+          </ul>
         </section>
+        </div>
       </article>
 
       {/* Vi har ju redan gjort en tidigare hämtning när man sökte receptet, kan ju skapa en suggestion-här då.
