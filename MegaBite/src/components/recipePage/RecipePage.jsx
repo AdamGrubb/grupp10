e93a12fd92b43f419d53904cef92956b9a07b23e
Grandelius <jonatan.grandelius@iths.se>
2023-04-19 16:09:19 +0200

    added icons to info section

--- MegaBite/src/components/recipePage/RecipePage.jsx
+++ MegaBite/src/components/recipePage/RecipePage.jsx
@@ -5,14 +5,25 @@ import "./RecipePage.css";
 export default function RecipePage() {
   
   const navigate = useNavigate();
+
+  let clock = <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+  </svg>;
+
+  let portions = <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z" />
+</svg>;
+
   
   const recipe = {
   title: "Oatmeal porridge",
-  ingredients: ["havregryn", "vatten", "salt", "salt", "salt", "salt","havregryn", "vatten", "salt", "salt", "salt"],
-  description: "Koka upp vatten och havregryn och rör om under tiden.",
+  ingredients: ["havregryn", "vatten", "salt", "salt", "salt", "salt","havregryn", "vatten", "salt", "salt", "salt",
+  "havregryn", "vatten", "salt", "salt", "salt"],
+  description: ["Koka upp vatten och havregryn och rör om under tiden.", "Koka upp vatten och havregryn och rör om under tiden."
+,"Koka upp vatten och havregryn och rör om under tiden.", "Koka upp vatten och havregryn och rör om under tiden."],
   time: 12,
   portions: 4,
-  allergies: ["egg", "vegan"]
+  allergens: ["egg", "vegan"]
 };
 
 return (
@@ -31,15 +42,16 @@ return (
         <h1 className="recipeTitle">{recipe.title}</h1>
         <section id="ServingsAndAllergens" className="">
           <div className="info bg-receptDescriptionColor">
-            <li>portions:{recipe.portions}portions:{recipe.portions}</li>
-            <li>allergies: {recipe.allergies}</li>
-            <li>time:{recipe.time}</li>
+            <div className="flex">
+            <p className="flex">{clock} &ensp; {recipe.time} Min</p>
+            <p className="flex portions">{portions} &ensp; {recipe.portions} Portions</p>
+            </div>
+            <p>Allergens: {recipe.allergens}</p>
             </div>
         </section>
 
         <section className="ingredients bg-receptDescriptionColor">
           <h2 id="big">Ingredients:</h2>
-          {/* <div style={{listStyle: 'disc'}}> */}-
         <div> 
           {recipe.ingredients.map(item => ( <li key={item}>{item}</li> ))}
            </div>
@@ -48,18 +60,20 @@ return (
         <section className= "description bg-receptDescriptionColor">
           <h2 id="big">Description:</h2>
           <div>
-            <li> {recipe.description}</li>
+            <li>{recipe.description.map(item => ( <li key={item}>{item}</li> ))}</li>
           </div>
         </section>
-        </div>
 
-      <section>
-      {<RecipeSuggestions/>}
-      </section>
+        <section className="recipeSuggestion">
+          <h2 id="big">Suggestions</h2>
+          <div>{<RecipeSuggestions/>}</div>
+            </section>
+
+        </div>
 
       </article>
 
 
     </>
-  );
+);
 }
