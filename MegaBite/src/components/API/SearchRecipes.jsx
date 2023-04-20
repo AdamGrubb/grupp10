6e55dca2be0b81f1e3ff0f7d852c323edea4596c
Johannes Ragnarsson <johannes.ragnarsson@gmail.com>
2023-04-20 09:31:01 +0200

    added branch for API dev

---
+++ MegaBite/src/components/API/SearchRecipes.jsx
@@ -0,0 +1,18 @@
+export async function SearchRecipes(queryParams) {
+  //https://api.spoonacular.com/recipes/complexSearch?query=pasta&maxFat=25&number=2
+  const apikey = "e00dc2fe3f314cee86a4eb59100267ef";
+  const baseUrl = "https://api.spoonacular.com/recipes/complexSearch";
+
+  const searchParams = new URLSearchParams();
+
+  searchParams.append("includeNutrition", "false");
+  searchParams.append("apikey", apikey);
+
+  const response = await fetch(`${baseUrl}?${searchParams}`);
+  const results = await response.json();
+  return results;
+}
+
+//   Object.keys(queryParams).forEach((key) => {
+//     searchParams.append(key, queryParams[key]);
+//   });
