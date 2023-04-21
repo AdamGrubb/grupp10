4645bb66a6d4c2160f959faf59e7f4f20c5db7e1
HUNDRA0 <109916846+HUNDRA0@users.noreply.github.com>
2023-04-21 10:11:37 +0200

    Added Try catch to Search

--- MegaBite/src/components/API/SearchRecipes.jsx
+++ MegaBite/src/components/API/SearchRecipes.jsx
@@ -1,21 +1,29 @@
 export async function SearchRecipes(queryParams) {
-  //https://api.spoonacular.com/recipes/complexSearch?query=pasta&maxFat=25&number=40
   const apikey = "e00dc2fe3f314cee86a4eb59100267ef";
   const baseUrl = "https://api.spoonacular.com/recipes/complexSearch";
+  
 
   const searchParams = new URLSearchParams();
+
+  console.log(queryParams); // Add this line to debug the issue
+
+  if (!queryParams || !queryParams.toString().trim()) {
+    alert("Please enter a search query.");
+    return null;
+  }
 
   searchParams.append("query", queryParams);
   searchParams.append("instructionsRequired", true);
   searchParams.append("apiKey", apikey);
   searchParams.append("number", 40);
-  
 
-  const response = await fetch(`${baseUrl}?${searchParams}`);
-  const results = await response.json();
-  return results;
+  try {
+    const response = await fetch(`${baseUrl}?${searchParams}`);
+    const results = await response.json();
+    return results;
+  } catch (error) {
+    console.error(error);
+    // Show an error message to the user using console.error() or a UI element
+    return null;
+  }
 }
-
-//   Object.keys(queryParams).forEach((key) => {
-//     searchParams.append(key, queryParams[key]);
-//   });
