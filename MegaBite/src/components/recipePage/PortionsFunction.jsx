c390cc12faa309ac3f7b28a81bedb2c3cef2d4fd
Grandelius <jonatan.grandelius@iths.se>
2023-04-21 09:39:10 +0200

    cleaned up PortionsFunction

--- MegaBite/src/components/recipePage/PortionsFunction.jsx
+++ MegaBite/src/components/recipePage/PortionsFunction.jsx
@@ -1,16 +1,13 @@
 export default function Portions(portions, amount) {
-  console.log(portions + " " + amount);
   if (portions == 4) {
-    return (amount = amount);
+    return amount;
   }
   if (portions == 6) {
-    return (amount = amount * 1.5);
+    return amount * 1.5;
   }
   if (portions == 8) {
-    return (amount = amount * 2);
+    return amount * 2;
   }
-
-  console.log(portions + " " + amount);
 
   return amount;
 }
