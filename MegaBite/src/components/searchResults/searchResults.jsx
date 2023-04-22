9f9437a6a096bc3549ce02aaba8af4a72eb1f558
Adam Grubb <adam.grubb.63@hotmail.com>
2023-04-22 19:05:06 +0200

    Add tailwind CSS and divs

--- MegaBite/src/components/searchResults/searchResults.jsx
+++ MegaBite/src/components/searchResults/searchResults.jsx
@@ -10,25 +10,35 @@ export default function SearchResults() {
   //Här ska jag ändra så att den tar emot en array av recept istället. Placeholdern är antal recept istället.
   const displayRecipeCards = (recipes) => {
     let RecipeCards = [];
-    //Här får du göra om till en map-funktion som stoppar in props till card så rätt bild och titel kommer in där. Sen att man skickar själva objektet till recipe-sidan med objekt via state.
     for (let x = 0; x < recipes; x++) {
       RecipeCards.push(
         <Link to="/searchresults/recipepage">
           <RecipeCard />
         </Link>
       );
-      console.log(x);
+      console.log("Hejh");
     }
     return RecipeCards;
+    //Här får du göra om till en map-funktion som stoppar in props till card så rätt bild och titel kommer in där. Sen att man skickar själva objektet till recipe-sidan med objekt via state.
   };
 
   return (
     <section className="">
-      <nav className="search-results">
-        <img className="arrow-img" src={arrow} alt="Previous" />
+      <nav className="flex flex-row flex-wrap justify-left">
+        {/* <img className="arrow-img" src={arrow} alt="Previous" /> */}
         {displayRecipeCards(recipeFromStore)}
-        <img className="arrow-img" src={arrow} alt="Next" />
+        {/* <img className="arrow-img" src={arrow} alt="Next" /> */}
       </nav>
+
+      {/* <div className="bg-slate-950 basis-28 text-center">Div 2</div>
+        <div className="bg-slate-950 basis-28 text-center">Div 3</div>
+        <div className="bg-slate-950 basis-28 text-center">Div 1</div>
+        <div className="bg-slate-950 basis-28 text-center">Div 4</div>
+        <div className="bg-slate-950 basis-28 text-center">Div 5</div>
+        <div className="bg-slate-950 basis-28 text-center">Div 6</div>
+        <div className="bg-slate-950 basis-28 text-center">Div 7</div>
+        <div className="bg-slate-950 basis-28 text-center">Div 8</div>
+        <div className="bg-slate-950 basis-28 text-center">Div 9</div> */}
     </section>
   );
 }
