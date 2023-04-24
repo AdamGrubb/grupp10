4823dfa7399cda116721d84d514bc2747506da32
Johannes Ragnarsson <johannes.ragnarsson@gmail.com>
2023-04-24 07:44:13 +0200

    added comments to the API calls

--- MegaBite/src/components/API/SearchRecipes.jsx
+++ MegaBite/src/components/API/SearchRecipes.jsx
@@ -1,30 +1,34 @@
 export async function SearchRecipes(queryParams) {
+  //API-key adds to the URL to be able to make API-calls
   const apikey = "e00dc2fe3f314cee86a4eb59100267ef";
+
+  //The base URL uses incoming recipeId and inserts it into the URL.
   const baseUrl = "https://api.spoonacular.com/recipes/complexSearch";
-  // const type = "breakfast";
 
-  const searchParams = new URLSearchParams();
+  // const type = "breakfast";     //Hardcoded for testing, will be deleted
 
-  console.log(queryParams); // Add this line to debug the issue
-
+  //If incoming variable is empty an error will be displayed
   if (!queryParams || !queryParams.toString().trim()) {
     alert("Please enter a search query.");
     return null;
   }
 
+  //URLSearchParams is used to manipulate the URL.
+  const searchParams = new URLSearchParams();
   searchParams.append("query", queryParams);
   searchParams.append("instructionsRequired", true);
-  // searchParams.append("type", type);
+  searchParams.append("type", type); //Define breakfast, lunch och dinner
   searchParams.append("apiKey", apikey);
   searchParams.append("number", 40);
 
+  //API call, if there is an error the error will be shown otherwise we return the data.
   try {
     const response = await fetch(`${baseUrl}?${searchParams}`);
     const results = await response.json();
     return results;
   } catch (error) {
-    console.error(error);
     // Show an error message to the user using console.error() or a UI element
+    console.error(error);
     return null;
   }
 }
--- MegaBite/src/components/API/SingleRecipe.jsx
+++ MegaBite/src/components/API/SingleRecipe.jsx
@@ -1,15 +1,27 @@
 export async function SingleRecipe(recipeId) {
+  //Example URL
   //   https://api.spoonacular.com/recipes/{id}/information
   //   https://api.spoonacular.com/recipes/716429/information?includeNutrition=false
+
+  //API-key adds to the URL to be able to make API-calls
   const apikey = "e00dc2fe3f314cee86a4eb59100267ef";
+
+  //The base URL uses incoming recipeId and inserts it into the URL.
   const baseUrl = `https://api.spoonacular.com/recipes/${recipeId}/information`;
 
+  //URLSearchParams is used to manipulate the URL.
   const searchParams = new URLSearchParams();
-
   searchParams.append("includeNutrition", false);
   searchParams.append("apiKey", apikey);
 
-  const response = await fetch(`${baseUrl}?${searchParams}`);
-  const results = await response.json();
-  return results;
+  //API call, if there is an error the error will be shown otherwise we return the data.
+  try {
+    const response = await fetch(`${baseUrl}?${searchParams}`);
+    const results = await response.json();
+    return results;
+  } catch (error) {
+    console.error(error);
+    // Show an error message to the user using console.error() or a UI element
+    return null;
+  }
 }
