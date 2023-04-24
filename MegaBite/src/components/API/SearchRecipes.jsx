ec4e51292b70765a55b26944014efaa50c02722c
Johannes Ragnarsson <johannes.ragnarsson@gmail.com>
2023-04-24 15:26:50 +0200

    added mapping for what to use from API

--- MegaBite/src/components/API/SearchRecipes.jsx
+++ MegaBite/src/components/API/SearchRecipes.jsx
@@ -5,7 +5,6 @@ export async function SearchRecipes(queryParams) {
   //The base URL uses incoming recipeId and inserts it into the URL.
   const baseUrl = "https://api.spoonacular.com/recipes/complexSearch";
 
-
   //If incoming variable is empty an error will be displayed
   if (!queryParams || !queryParams.toString().trim()) {
     alert("Please enter a search query.");
@@ -17,14 +16,30 @@ export async function SearchRecipes(queryParams) {
   searchParams.append("query", queryParams);
   searchParams.append("instructionsRequired", true);
   searchParams.append("apiKey", apikey);
-  searchParams.append("number", 40);
-  searchParams.append("addRecipeInformation", true)
+  searchParams.append("number", 2);
+  searchParams.append("addRecipeInformation", true);
+  searchParams.append("fillIngredients", true);
 
   //API call, if there is an error the error will be shown otherwise we return the data.
   try {
     const response = await fetch(`${baseUrl}?${searchParams}`);
-    const results = await response.json();
-    return results;
+    const apiResults = await response.json();
+    console.log(apiResults);
+    const recipeArray = apiResults.results.map((item) => ({
+      id: item.id,
+      title: item.title,
+      allergens: [
+        { glutenFree: item.glutenFree },
+        { lactose: item.dairyFree },
+        { vegetarian: item.vegetarian },
+        { vegan: item.vegan },
+      ],
+      servings: item.servings,
+      description: [...item.analyzedInstructions[0].steps],
+      ingredients: [...item.extendedIngredients],
+      img: item.image,
+    }));
+    return recipeArray;
   } catch (error) {
     // Show an error message to the user using console.error() or a UI element
     console.error(error);
