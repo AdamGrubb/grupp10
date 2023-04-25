3670c57690276a3dd227a2f6971acfb547822fbf
Johannes Ragnarsson <johannes.ragnarsson@gmail.com>
2023-04-25 09:55:16 +0200

    .

--- MegaBite/src/components/search/Search.jsx
+++ MegaBite/src/components/search/Search.jsx
@@ -2,27 +2,27 @@ import { useNavigate, useSearchParams } from "react-router-dom";
 import "./Search.css";
 import magnGlass from "../../assets/magnGlass.png";
 import { SearchRecipes } from "../API/SearchRecipes";
+import AlertPopup from "../Utilities/Alert";
 import React, { useState } from "react";
 
 export default function Search() {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const [showFilters, setShowFilters] = useState(false);
-  const [glutenFree, setGlutenFree] = useState(false);
-  const [dairyFree, setDairyFree] = useState(false);
+  const [searchword, setSearchword] = useState("");
+
   const [recipeData, setRecipeData] = useState([]);
-  const [vegan, setVegan] = useState(false);
   const navigate = useNavigate();
 
-  const handleChange = (event) => {
-    setSearchParams(event.target.value.toLowerCase());
+  const handleChange = (e) => {
+    setSearchword(e.target.value.toLowerCase());
   };
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    event.target.reset();
-    const data = await SearchRecipes(searchParams);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(searchword);
+    const data = await SearchRecipes(searchword);
     setRecipeData(data);
-    console.log(recipeData);
+    console.log(data);
+    e.target.reset();
+    setSearchword("");
   };
 
   return (
@@ -37,61 +37,12 @@ export default function Search() {
               placeholder="Search..."
             />
             <button
-              className="absolute top-0 right-0 h-full px-4 text-sm text-gray-500"
-              onClick={() => setShowFilters(!showFilters)}
-            >
-              Filters
-            </button>
-            <button
               type="submit"
-              onClick={() => {
-                navigate("/searchresults");
-              }}
               className="absolute btnSearch mt-7 top-0 right-0 h-full px-4 text-sm border-2 border-solid"
             >
               Search
             </button>
           </form>
-          {showFilters && (
-            <div
-              className="absolute top-full left-0 right-0 px-4 py-2 border rounded shadow"
-              style={{ backgroundColor: "gray", zIndex: 1 }}
-            >
-              <div className="flex items-center">
-                <input
-                  type="checkbox"
-                  id="gluten-free"
-                  checked={glutenFree}
-                  onChange={(e) => setGlutenFree(e.target.checked)}
-                />
-                <label htmlFor="gluten-free" className="ml-2">
-                  Gluten-free
-                </label>
-              </div>
-              <div className="flex items-center">
-                <input
-                  type="checkbox"
-                  id="dairy-free"
-                  checked={dairyFree}
-                  onChange={(e) => setDairyFree(e.target.checked)}
-                />
-                <label htmlFor="dairy-free" className="ml-2">
-                  Dairy-free
-                </label>
-              </div>
-              <div className="flex items-center">
-                <input
-                  type="checkbox"
-                  id="vegan"
-                  checked={vegan}
-                  onChange={(e) => setVegan(e.target.checked)}
-                />
-                <label htmlFor="vegan" className="ml-2">
-                  Vegan
-                </label>
-              </div>
-            </div>
-          )}
         </div>
       </div>
     </section>
