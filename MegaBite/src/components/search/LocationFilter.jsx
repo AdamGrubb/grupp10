924954c1564db37a3e4a024ec1fad2c6a2ad5fed
KiaZA91 <111753874+KiaZA91@users.noreply.github.com>
2023-05-02 11:18:36 +0200

    Drop-down list with scrollbar

--- MegaBite/src/components/search/LocationFilter.jsx
+++ MegaBite/src/components/search/LocationFilter.jsx
@@ -1,25 +1,69 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
 export default function Location() {
-  const [location, setLocation] = useState("Denna ska ändras");
+  const [isOpen, setIsOpen] = useState(false);
+  const [size, setSize] = useState(1);
+  const dropdownRef = useRef(null);
+
+  const handleClickOutside = (event) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+      setIsOpen(false);
+    }
+  };
+
+  const locationsArray = [
+    { value: "", displayText: "All Locations" },
+    { value: "African", displayText: "African" },
+    { value: "American", displayText: "American" },
+    { value: "Chinese", displayText: "Chinese" },
+    { value: "European", displayText: "European" },
+    { value: "Greek", displayText: "Greek" },
+    { value: "Japanese", displayText: "Japanese" },
+    { value: "Mexican", displayText: "Mexican" },
+    { value: "Middle Eastern", displayText: "Middle Eastern" },
+    { value: "Thai", displayText: "Thai" },
+  ];
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  useEffect(() => {
+    setSize(isOpen ? 5 : 1);
+  }, [isOpen]);
+
+  const handleClick = (e) => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleChange = (e) => {
+    setLocation(e.target.value);
+    setIsOpen(false);
+  };
+
   return (
     <>
       <section className="btnSearch mt-2 mr-12 justify-self-start text-sm border-2 border-solid">
         <select
-          className="bg-receptDescriptionColor "
-          onChange={(e) => setLocation(e.target.value)}
+          ref={dropdownRef}
+          className="bg-receptDescriptionColor"
+          onChange={handleChange}
+          size={size}
+          onClick={handleClick}
+          style={{
+            overflowY: isOpen ? "auto" : "hidden",
+            height: "auto",
+            maxHeight: isOpen ? "calc(1.6em * 5 + 0.25em * 2)" : "auto",
+          }}
         >
-          <option value={""}>All Locations</option>
-          <option value={"African"}>African</option>
-          <option value={"American"}>American</option>
-          <option value={"Chinese"}>Chinese</option>
-          <option value={"European"}>European</option>
-          <option value={"Greek"}>Greek</option>
-          <option value={"Japanese"}>Japanese</option>
-          <option value={"Mexican"}>Mexican</option>
-          <option value={"Middle Eastern"}>Middle Eastern</option>
-          <option value={"Thai"}>Thai</option>
-          Locations
+          {locationsArray.map((location) => (
+            <option key={location.value} value={location.value}>
+              {location.displayText}
+            </option>
+          ))}
         </select>
       </section>
     </>
