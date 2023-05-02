1eb0c2100b8a063f9cea662801801f79dcb9b2c7
Adam Grubb <adam.grubb.63@hotmail.com>
2023-05-02 11:14:15 +0200

    Katarina Ramer - Remove comments and implement mealtype-filter

--- MegaBite/src/components/search/Filter.jsx
+++ MegaBite/src/components/search/Filter.jsx
@@ -1,71 +1,39 @@
- import useRecipeStore from "../../hooks/useRecipeStore";
- 
- export const Filter = ( recipes, filters
-    // glutenFree,
-    // dairyFree,
-    // vegetarian,
-    // vegan,
-    // breakfast,
-    // lunch,
-    // dinner
-  ) => {
+export const Filter = (recipes, filters) => {
+  const filteredStore = [];
 
-    // const addFilteredRecipes = useRecipeStore((state) => state.addFilteredRecipes);
-  
-    // const recipeCollection = useRecipeStore(
-    //   (state) => state.recipeCollection
-    // );
-    //Get the API-data stored in store
-  
-    const filteredStore = [];
-  
-    // const glutenFree = false;
-    // const dairyFree = false;
-    // const vegetarian = false;
-    // const vegan = false;
-  
-    // const breakfast = true;
-    // const lunch = false;
-    // const dinner = false;
-    // const allMealtypes = !breakfast && !lunch && !dinner;
-  
-    //breakfast && recipe.mealType.includes("breakfast")
-    recipes.forEach((recipe) => {
-      if (filters.glutenFree && !recipe.allergens[0].glutenFree) {
-        return;
-      }
-      if (filters.dairyFree && !recipe.allergens[1].lactose) {
-        return;
-      }
-      if (filters.vegetarian && !recipe.allergens[2].vegetarian) {
-        return;
-      }
-      if (filters.vegan && !recipe.allergens[3].vegan) {
-        return;
-      }
-      if (filters.breakfast && recipe.mealType[0].breakfast) {
-        filteredStore.push(recipe);
-        return;
-      }
-      if (filters.lunch && recipe.mealType[1].lunch) {
-        filteredStore.push(recipe);
-        return;
-      }
-      if (filters.dinner && recipe.mealType[2].dinner) {
-        filteredStore.push(recipe);
-        return;
-      }
+  const allMealtypes = !filters.breakfast && !filters.lunch && !filters.dinner;
+
+  recipes.forEach((recipe) => {
+    if (filters.glutenFree && !recipe.allergens[0].glutenFree) {
+      return;
+    }
+    if (filters.dairyFree && !recipe.allergens[1].lactose) {
+      return;
+    }
+    if (filters.vegetarian && !recipe.allergens[2].vegetarian) {
+      return;
+    }
+    if (filters.vegan && !recipe.allergens[3].vegan) {
+      return;
+    }
+    if (filters.breakfast && recipe.mealType.includes("breakfast")) {
       filteredStore.push(recipe);
-      // if (allMealtypes) {
-      //   filteredStore.push(recipe);
-      //   return;
-      // }
-    });
-    // console.log("updated filtered store", filteredStore);
-    // addFilteredRecipes(filteredStore);
-    return filteredStore;
-  }
-  
-  export default Filter;
+      return;
+    }
+    if (filters.lunch && recipe.mealType.includes("lunch")) {
+      filteredStore.push(recipe);
+      return;
+    }
+    if (filters.dinner && recipe.mealType.includes("dinner")) {
+      filteredStore.push(recipe);
+      return;
+    }
+    if (allMealtypes) {
+      filteredStore.push(recipe);
+      return;
+    }
+  });
+  return filteredStore;
+};
 
- 
+export default Filter;
