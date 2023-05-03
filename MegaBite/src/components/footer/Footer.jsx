0eb1682011f7d9ff1adeb7a4fb4a7e77d1e512cd
HUNDRA0 <109916846+HUNDRA0@users.noreply.github.com>
2023-05-03 10:34:43 +0200

    Added LinkedIn to footer

--- MegaBite/src/components/footer/Footer.jsx
+++ MegaBite/src/components/footer/Footer.jsx
@@ -1,4 +1,5 @@
 import "../../index.css";
+import "./Footer.css";
 
 export function Footer() {
   return (
@@ -6,8 +7,14 @@ export function Footer() {
       <footer className="bg-footerColor footer">
         <div>
           <h3>FOOTER</h3>
+          <p className="LinkedIn">
+          <a href="https://grupp10.carrd.co/" target="_blank" rel="noopener noreferrer">
+              LinkedIn
+            </a>
+          </p>
         </div>
       </footer>
+
     </>
   );
 }
