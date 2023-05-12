55191370dd0b4ca6269085d727c723355186a5b7
KiaZA91 <111753874+KiaZA91@users.noreply.github.com>
2023-05-12 14:51:41 +0200

    added Cuisines

---
+++ MegaBite/src/components/API/Cuisines.jsx
@@ -0,0 +1,108 @@
+const cuisines = [
+  {
+    cuisine: "African",
+    countries: ["Gambia", "Somalia", "Kenya", "Senegal"],
+  },
+  {
+    cuisine: "American",
+    countries: ["United States"],
+  },
+  {
+    cuisine: "British",
+    countries: ["United Kingdom"],
+  },
+  {
+    cuisine: "Cajun",
+    countries: ["United States"],
+  },
+  {
+    cuisine: "Caribbean",
+    countries: ["Jamaica", "Cuba", "Dominican Republic", "Trinidad and Tobago"],
+  },
+  {
+    cuisine: "Chinese",
+    countries: ["China"],
+  },
+  {
+    cuisine: "Eastern European",
+    countries: ["Russia", "Poland", "Hungary", "Romania"],
+  },
+  {
+    cuisine: "European",
+    countries: ["Various"],
+  },
+  {
+    cuisine: "French",
+    countries: ["France"],
+  },
+  {
+    cuisine: "German",
+    countries: ["Germany"],
+  },
+  {
+    cuisine: "Greek",
+    countries: ["Greece"],
+  },
+  {
+    cuisine: "Indian",
+    countries: ["India"],
+  },
+  {
+    cuisine: "Irish",
+    countries: ["Ireland"],
+  },
+  {
+    cuisine: "Italian",
+    countries: ["Italy"],
+  },
+  {
+    cuisine: "Japanese",
+    countries: ["Japan"],
+  },
+  {
+    cuisine: "Jewish",
+    countries: ["Israel"],
+  },
+  {
+    cuisine: "Korean",
+    countries: ["South Korea"],
+  },
+  {
+    cuisine: "Latin American",
+    countries: ["Mexico", "Brazil", "Argentina", "Peru"],
+  },
+  {
+    cuisine: "Mediterranean",
+    countries: ["Various"],
+  },
+  {
+    cuisine: "Mexican",
+    countries: ["Mexico"],
+  },
+  {
+    cuisine: "Middle Eastern",
+    countries: ["Lebanon", "Turkey", "Israel"],
+  },
+  {
+    cuisine: "Nordic",
+    countries: ["Sweden", "Denmark", "Norway"],
+  },
+  {
+    cuisine: "Southern",
+    countries: ["United States"],
+  },
+  {
+    cuisine: "Spanish",
+    countries: ["Spain"],
+  },
+  {
+    cuisine: "Thai",
+    countries: ["Thailand"],
+  },
+  {
+    cuisine: "Vietnamese",
+    countries: ["Vietnam"],
+  },
+];
+
+export default cuisines;
