b6e2e0d62fdf4df8423345189000e8bd77d1faeb
HUNDRA0 <109916846+HUNDRA0@users.noreply.github.com>
2023-05-14 20:46:52 +0200

    Small fix

--- MegaBite/src/components/search/LocationFilter.jsx
+++ MegaBite/src/components/search/LocationFilter.jsx
@@ -1,9 +1,10 @@
 import { useState, useEffect, useRef } from "react";
 import "./LocationFilter.css";
 
-export default function Location(props) {
+export default function LocationFilter(props) {
   const [isOpen, setIsOpen] = useState(false);
-  const [selectedLocation, setSelectedLocation] = useState("All Locations"); // visar location
+  const [selectedLocation, setSelectedLocation] = useState("All Locations");
+  const [currentCoordinates, setCurrentCoordinates] = useState(null); // State för koordinaterna
   const dropdownRef = useRef(null);
 
   const handleClickOutside = (event) => {
@@ -35,6 +36,37 @@ export default function Location(props) {
   const handleClick = (e) => {
     setIsOpen(!isOpen);
   };
+
+  const handleLocationClick = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        async (position) => {
+          try {
+            const { latitude, longitude } = position.coords;
+            props.setLocation({ latitude, longitude });
+            setSelectedLocation("My Location");
+            setCurrentCoordinates({ latitude, longitude }); // Sätter koordinaterna i state
+
+            const response = await fetch(
+              `https://api.mapbox.com/geocoding/v5/mapbox.places/${longitude},${latitude}.json?access_token=pk.eyJ1IjoiaHVuZHJhMCIsImEiOiJjbGhucXg2N3gxb3d5M3FudTdjYXhodXMwIn0.CoCcBmeZ7hOkSU9ujWVPNw`
+            );
+            const data = await response.json();
+            const placeName = data.features[0].place_name;
+            console.log('User location:', placeName);
+            // Do something with the location information
+          } catch (error) {
+            console.error('Error getting location information:', error);
+          }
+        },
+        (error) => {
+          console.error('Error getting location:', error);
+        }
+      );
+    } else {
+      console.error('Geolocation is not supported by this browser.');
+    }
+  };
+
   const handleChange = (location) => {
     props.setLocation(location.value);
     setSelectedLocation(location.displayText);
@@ -43,29 +75,27 @@ export default function Location(props) {
 
   return (
     <>
-      <section
-        className="dropdown-section mt-2 mr-12 justify-self-start text-sm border-2 border-solid dropdown-container dropdown-wrapper"
-        ref={dropdownRef}
-      >
+      <section className="dropdown-section mt-2 mr-12 justify-self-start text-sm border-2 border-solid dropdown-container dropdown-wrapper" ref={dropdownRef}>
         <div className="dropdown-select" onClick={handleClick}>
           {selectedLocation}
         </div>
         {isOpen && (
-          <ul className="dropdown-list">
-            {locationsArray.map((location) => (
-              <li key={location.value} className="checkbox-item">
-                <button
-                  className="btnRegion"
-                  type="button"
-                  onClick={() => handleChange(location)} // Removed 'value' attribute
-                >
-                  {location.displayText}
-                </button>
-              </li>
-            ))}
-          </ul>
-        )}
-      </section>
-    </>
-  );
+           <ul className="dropdown-list">
+           <li key="my-location" className="checkbox-item">
+             <button className="btnRegion" type="button" onClick={handleLocationClick}>
+               Use My Location
+             </button>
+           </li>
+           {locationsArray.map((location) => (
+             <li key={location.value} className="checkbox-item">
+               <button className="btnRegion" type="button" onClick={() => handleChange(location)}>
+                 {location.displayText}
+               </button>
+             </li>
+           ))}
+         </ul>
+       )}
+     </section>
+   </>
+ );
 }
