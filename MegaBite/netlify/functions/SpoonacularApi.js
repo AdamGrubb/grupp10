5c0617d3fdb23661eb68600e82ba502cf9bffd5b
Adam Grubb <adam.grubb.63@hotmail.com>
2023-05-15 14:25:28 +0200

    Att try catch to SpoonacularApi

--- MegaBite/netlify/functions/SpoonacularApi.js
+++ MegaBite/netlify/functions/SpoonacularApi.js
@@ -1,13 +1,27 @@
 exports.handler = async function (event, context) {
   const baseUrl = "https://api.spoonacular.com/recipes/complexSearch";
-  console.log(event.queryStringParameters);
+try {
   const querys = new URLSearchParams(event.queryStringParameters);
   querys.append("apiKey", process.env.SPOONACULAR_APIKEY_STRAND);
   const response = await fetch(`${baseUrl}?${querys}`);
-  const returnResult = await response.json();
-  console.log(returnResult);
-  return {
-    statusCode: 200,
-    body: JSON.stringify(returnResult),
-  };
+  const responseStatus = response.status;
+  if (response.ok)
+  {
+    const returnResult = await response.json();
+    console.log(returnResult);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(returnResult),
+    };
+  }
+return {
+  statusCode: responseStatus, 
+}
+}
+catch (error) {
+  return{
+    statusCode: 500
+  }
+}
+
 };
