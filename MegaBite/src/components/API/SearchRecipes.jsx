85538fd80733ae61fea5b9a288cc1ac409ffba43
Adam Grubb <adam.grubb.63@hotmail.com>
2023-05-15 13:44:38 +0200

    Implement Netlfiy in SearcRecpes

--- MegaBite/src/components/API/SearchRecipes.jsx
+++ MegaBite/src/components/API/SearchRecipes.jsx
@@ -7,11 +7,10 @@ export async function SearchRecipes(queryParams) {
   //29f9e7adbc11492184269307e04a5dad JONATHAN S
 
   //The base URL uses incoming recipeId and inserts it into the URL.
-  const baseUrl = "https://api.spoonacular.com/recipes/complexSearch";
+  const baseUrl = "/.netlify/functions/SpoonacularApi";
 
   //URLSearchParams is used to manipulate the URL.
   const searchParams = new URLSearchParams();
-  searchParams.append("apiKey", apikey);
   searchParams.append("query", queryParams);
   searchParams.append("instructionsRequired", true);
   searchParams.append("number", 50);
