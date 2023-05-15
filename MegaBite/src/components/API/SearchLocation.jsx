e6d832f5ba87e89585b84d683799f13cb3ccef1d
katarinaramer <katarina.ramer@iths.se>
2023-05-15 09:19:35 +0200

    Error handling added

--- MegaBite/src/components/API/SearchLocation.jsx
+++ MegaBite/src/components/API/SearchLocation.jsx
@@ -1,13 +1,13 @@
-export async function SearchLocation() {
-    const apikey = "005ff50ab67648419887f11cca86bfe8";
+export async function SearchLocation(coordinateIncomingParams) {
+    const apikey = "005ff50ab67648419887f11cca86bfe";
+    //8
   
 //undefined koordinater: [{Latitude: -15.618611}, {Longitude: -9.868314}] 
 //koordinater: [{Latitude: 55.4993}, {Longitude: 9.7307}]
-const coordinateIncomingParams = [{Latitude: -15.618611}, {Longitude: -9.868314}];
+//const coordinateIncomingParams = [{Latitude: 55.4993}, {Longitude: 9.7307}];
 const latitude = coordinateIncomingParams[0].Latitude;
 const longitude = coordinateIncomingParams[1].Longitude;
 const coordinateParams = latitude + "+" + longitude;
-//console.log(coordinateParams);
 
     //https://api.opencagedata.com/geocode/v1/json?q=LAT+LNG&key=YOUR-API-KEY
     const baseUrl = "https://api.opencagedata.com/geocode/v1/json?";
@@ -18,6 +18,9 @@ const coordinateParams = latitude + "+" + longitude;
   
     try {
       const response = await fetch(`${baseUrl}${searchParams}`);
+      if (!response.ok) {
+        alert(`HTTP error! status: ${response.status} Check console for response message.`);
+      }
       const apiResults = await response.json();
       const country = apiResults.results[0].components.country;
       const continent = apiResults.results[0].components.continent;
@@ -27,12 +30,23 @@ const coordinateParams = latitude + "+" + longitude;
       }
 
       const geoLocation = {country, continent};
-
+    
       return geoLocation;
 
-    } catch (error) {
-      console.error(error);
-      return {};
+    } catch(error)
+     {
+      console.log(error + `
+      400	Invalid request (bad request; a required parameter is missing; invalid coordinates; invalid version; invalid format)
+      401	Unable to authenticate - missing, invalid, or unknown API key
+      402	Valid request but quota exceeded (payment required)
+      403	Forbidden (API key disabled or IP address rejected)
+      404	Invalid API endpoint
+      405	Method not allowed (non-GET request)
+      408	Timeout; you can try again
+      410	Request too long
+      426	Upgrade required (unsupported TLS)
+      429	Too many requests (too quickly, rate limiting)
+      503	Internal server error`)
     }
   }
   
