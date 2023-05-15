71a1a1e6a14fc071077920ff4b97ef86dc457d71
HUNDRA0 <109916846+HUNDRA0@users.noreply.github.com>
2023-05-15 22:48:23 +0200

    Updated function

--- MegaBite/src/components/search/LocationComponent.jsx
+++ MegaBite/src/components/search/LocationComponent.jsx
@@ -1,45 +1,26 @@
-// import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
-// const LocationComponent = (props) => {
-//   const [location, setLocation] = useState(null);
+const LocationComponent = (props) => {
+  const [location, setLocation] = useState(null);
 
-//   useEffect(() => {
-//     if (navigator.geolocation) {
-//       navigator.geolocation.getCurrentPosition(
-//         position => {
-//           const { latitude, longitude } = position.coords;
-//           setLocation({ latitude, longitude });
-//         },
-//         error => {
-//           console.error('Error getting location:', error);
-//         }
-//       );
-//     } else {
-//       console.error('Geolocation is not supported by this browser.');
-//     }
-//   }, []);
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          const { latitude, longitude } = position.coords;
+          setLocation({ latitude, longitude });
+          props.setLocation({ latitude, longitude }); // Skicka koordinaterna till överordnade komponenten
+        },
+        error => {
+          console.error('Error getting location:', error);
+        }
+      );
+    } else {
+      console.error('Geolocation is not supported by this browser.');
+    }
+  }, [props]);
 
-//   const getLocationInfo = async () => {
-//     if (location) {
-//       try {
-//         const response = await fetch(
-//           `https://api.mapbox.com/geocoding/v5/mapbox.places/${location.longitude},${location.latitude}.json?access_token=pk.eyJ1IjoiaHVuZHJhMCIsImEiOiJjbGhucXg2N3gxb3d5M3FudTdjYXhodXMwIn0.CoCcBmeZ7hOkSU9ujWVPNw`
-//         );
-//         const data = await response.json();
-//         const placeName = data.features[0].place_name;
-//         console.log('User location:', placeName);
-//         // Do something with the location information
-//       } catch (error) {
-//         console.error('Error getting location information:', error);
-//       }
-//     }
-//   };
+  return null;
+};
 
-//   return (
-//     <div>
-//       <button onClick={getLocationInfo}>Get Location</button>
-//     </div>
-//   );
-// };
-
-// export default LocationComponent;
+export default LocationComponent;
