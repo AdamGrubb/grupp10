72484376fd139571ceb8add7cdf294a19589784a
Adam Grubb <adam.grubb.63@hotmail.com>
2023-05-16 19:49:35 +0200

    Add comments in netlify functions

--- MegaBite/netlify/functions/SpoonacularApi.js
+++ MegaBite/netlify/functions/SpoonacularApi.js
@@ -1,9 +1,14 @@
 exports.handler = async function (event, context) {
   const baseUrl = "https://api.spoonacular.com/recipes/complexSearch?";
 try {
+  //Saves the querys sent from front-end call.
   const querys = new URLSearchParams(event.queryStringParameters);
+
+  //Get the API-key from .env file.
   querys.append("apiKey", process.env.SPOONACULAR_APIKEY);
   const response = await fetch(`${baseUrl}${querys}`);
+
+  //Saves the responsstatus to be sent with a return if the API-call fails.
   const responseStatus = response.status;
   if (response.ok)
   {
