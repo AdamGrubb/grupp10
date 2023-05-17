b87c49be64fff1dd6e9a5763056c81f2b96ea1df
Jonathan Strand <jontepwn@gmail.com>
2023-05-17 13:34:14 +0200

    modified header and footer color

--- MegaBite/tailwind.config.js
+++ MegaBite/tailwind.config.js
@@ -3,14 +3,14 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     colors: {
-      headerColor: "#272727",
+      headerColor: "#25252b",
       searchFilterAreaColor: "#ADD37D",
       grayColor: "#e5e8ea",
       locationDropdownColor: "#AAD37D",
       receptDescriptionColor: "#AAD37D",
       foodNamebarColor: "#AAD37D",
       backgroundColor: "#E9EAE5",
-      footerColor: "#272727",
+      footerColor: "#25252b",
     },
     extend: {},
   },
