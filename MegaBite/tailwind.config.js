97c6d8a560f3835d72ea7eb837db2095146fcad8
Jonathan Strand <jontepwn@gmail.com>
2023-05-17 12:05:57 +0200

    update

--- MegaBite/tailwind.config.js
+++ MegaBite/tailwind.config.js
@@ -3,14 +3,14 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     colors: {
-      headerColor: "#CCE0C4",
+      headerColor: "#272727",
       searchFilterAreaColor: "#ADD37D",
-      grayColor: "#E8EAE5",
+      grayColor: "#e5e8ea",
       locationDropdownColor: "#AAD37D",
       receptDescriptionColor: "#AAD37D",
       foodNamebarColor: "#AAD37D",
       backgroundColor: "#E9EAE5",
-      footerColor: "#CCE0C4",
+      footerColor: "#272727",
     },
     extend: {},
   },
