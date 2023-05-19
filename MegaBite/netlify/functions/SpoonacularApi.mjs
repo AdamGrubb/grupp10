fde54132c8e988383817815ae00297780c15167e
Adam Grubb <adam.grubb.63@hotmail.com>
2023-05-19 11:05:19 +0200

    Remove Spoonacular.js and remove console.log in Spoonacular.mjs

--- MegaBite/netlify/functions/SpoonacularApi.mjs
+++ MegaBite/netlify/functions/SpoonacularApi.mjs
@@ -1,41 +1,31 @@
-import fetch from 'node-fetch';
+import fetch from "node-fetch";
 
 export const handler = async function (event, context) {
   const baseUrl = "https://api.spoonacular.com/recipes/complexSearch?";
-try {
-  //Saves the querys sent from front-end call.
-  const querys = new URLSearchParams(event.queryStringParameters);
+  try {
+    //Saves the querys sent from front-end call.
+    const querys = new URLSearchParams(event.queryStringParameters);
 
-  //Get the API-key from .env file.
-  querys.append("apiKey", process.env.SPOONACULAR_APIKEY);
-   console.log(querys)
-    console.log(`${baseUrl}${querys}`)
-  const response = await fetch(`${baseUrl}${querys}`);
-  console.log(response)
+    //Get the API-key from .env file.
+    querys.append("apiKey", process.env.SPOONACULAR_APIKEY);
+    const response = await fetch(`${baseUrl}${querys}`);
 
-  console.log(response)
-
-  //Saves the responsstatus to be sent with a return if the API-call fails.
-  const responseStatus = response.status;
-  if (response.ok)
-  {
-    const returnResult = await response.json();
+    //Saves the responsstatus to be sent with a return if the API-call fails.
+    const responseStatus = response.status;
+    if (response.ok) {
+      const returnResult = await response.json();
+      return {
+        statusCode: 200,
+        body: JSON.stringify(returnResult),
+      };
+    }
     return {
-      statusCode: 200,
-      body: JSON.stringify(returnResult),
+      statusCode: responseStatus,
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      statusCode: 500,
     };
   }
-return {
-  statusCode: responseStatus, 
-}
-}
-catch (error) {
-  console.log(error)
-  return{
-    statusCode: 500
-  }
-}
-
 };
-
-
