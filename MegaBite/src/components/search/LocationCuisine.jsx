30b2c0c0bf12ec4c745fd745a018c97520ab4f2e
Grandelius <jonatan.grandelius@iths.se>
2023-05-22 10:43:32 +0200

    Cleanup and new file with locationArray

--- MegaBite/src/components/search/LocationCuisine.jsx
+++ MegaBite/src/components/search/LocationCuisine.jsx
@@ -1,10 +1,13 @@
 import getCuisineByCountry from "../CuisinesByLocation/CuisineFeature";
 import { SearchLocation } from "../API/SearchLocation";
-import LocationComponent from "./LocationComponent";
+import GetCoordinates from "./LocationCoordinates";
 
+/* FUNCTION IN THREE STEPS
+step 1 - get the users coordinates
+step 2 - get country by using the coodinates
+step 3 - get result (cuisine) by using country into the cuisines filter */
 export async function LocationCuisine() {
-  const coordinates = await LocationComponent();
-
+  const coordinates = await GetCoordinates();
   const country = await SearchLocation(coordinates);
   const result = getCuisineByCountry(country);
 
