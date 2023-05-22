92d937476b430a948793377e77879e0188f3ee37
katarinaramer <katarina.ramer@iths.se>
2023-05-22 13:46:49 +0200

    Clean up done

--- MegaBite/src/components/recipeCard/RecipeCard.jsx
+++ MegaBite/src/components/recipeCard/RecipeCard.jsx
@@ -2,8 +2,8 @@ import "./RecipeCard.css";
 
 export default function RecipeCard(props) {
   return (
-    <div key={props.id} className="recipeCard m-2" title= {props.title}>
-      <img src={props.img} alt="recipe thumbnail" className="recipeThumb"/>
+    <div key={props.id} className="recipeCard m-2" title={props.title}>
+      <img src={props.img} alt="recipe thumbnail" className="recipeThumb" />
       <div className="recipe-overlay">
         <div className="recipe-label"> {props.title}</div>
       </div>
